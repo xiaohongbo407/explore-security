b0caa7957c8ae4109fb49545e2af4909456b8f72
xiaohongbo <xiaohongbo407@163.com>
2018-01-08 20:36:46 +0800

    init commit

--- explore-security-browser/src/main/java/com/explore/security/browser/BrowserSecurityConfig.java
+++ explore-security-browser/src/main/java/com/explore/security/browser/BrowserSecurityConfig.java
@@ -1,17 +1,28 @@
 package com.explore.security.browser;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by xiaohb on 2018/1/8.
  */
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
+//        http.httpBasic()
                 .and()
                 .authorizeRequests()
                 .anyRequest()
